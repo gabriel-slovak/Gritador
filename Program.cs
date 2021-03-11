eeb79aab8bc63e63c73f47fa8f4aac7bb074566c
gabriel-slovak <slovakgabriellopes@gmail.com>
2021-03-11 15:31:29 -0300

    Finalização do programa Gritador

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Gritador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string qualquerCoisa;
+        Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("----------------------");
+        Console.ResetColor();
+            Console.WriteLine("Realizando o Gritador!");
+        Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("----------------------");
+        Console.ResetColor();
+            Console.WriteLine();
+
+            Console.Write("Digite qualquer coisa: ");
+            qualquerCoisa = Console.ReadLine();
+            Console.Write("\n");
+
+        Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"{qualquerCoisa}!!!".ToUpper());
+        Console.ResetColor();
+            Console.WriteLine();
+
+
+        }
+    }
+}
