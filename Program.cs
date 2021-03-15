b17dfc27b3a97dbc43c8ce71166bfa5ed7cdb4b4
Diego Neri <diego@diegoneri.com>
2021-03-15 17:02:04 -0300

    Ajusta endentação

--- Program.cs
+++ Program.cs
@@ -7,22 +7,22 @@ namespace Gritador
         static void Main(string[] args)
         {
             string qualquerCoisa;
-        Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("----------------------");
-        Console.ResetColor();
+            Console.ResetColor();
             Console.WriteLine("Realizando o Gritador!");
-        Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("----------------------");
-        Console.ResetColor();
+            Console.ResetColor();
             Console.WriteLine();
 
             Console.Write("Digite qualquer coisa: ");
             qualquerCoisa = Console.ReadLine();
             Console.Write("\n");
 
-        Console.ForegroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine($"{qualquerCoisa}!!!".ToUpper());
-        Console.ResetColor();
+            Console.ResetColor();
             Console.WriteLine();
 
 
